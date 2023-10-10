c96230dc3a7a0f5ce6f455fddf17c17a39bafb6d
Liya Josh <liyajosh07@gmail.com>
2023-10-10 09:16:55 +0530

    Added test configurations

--- src/test/java/com/project/crystalBall/CrystalBallApplicationTests.java
+++ src/test/java/com/project/crystalBall/CrystalBallApplicationTests.java
@@ -1,9 +1,14 @@
 package com.project.crystalBall;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class CrystalBallApplicationTests {
 
 	@Test
