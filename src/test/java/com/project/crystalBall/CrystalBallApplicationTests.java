6e12cd0971a8863a5e8962c6314e02ca5356d37d
Liya Josh <liyajosh07@gmail.com>
2023-10-16 09:14:41 +0530

    Added context for test cases

--- src/test/java/com/project/crystalBall/CrystalBallApplicationTests.java
+++ src/test/java/com/project/crystalBall/CrystalBallApplicationTests.java
@@ -1,9 +1,13 @@
 package com.project.crystalBall;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class CrystalBallApplicationTests {
 
 	@Test
